00d83fed35b25aec7006f5fd84d49ae1a03a7e78
artChistyakov <tomsonone08@gmail.com>
2026-05-02 22:19:30 +0300

    Lesson 31. Chat lesson 2

--- static/index.js
+++ static/index.js
@@ -1,1 +1,27 @@
-alert("Welcome to my chat!");
+const socket = io();
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (input.value) {
+        socket.emit('new_message', input.value);
+        input.value = '';
+    }
+});
+
+socket.on('message', function(msg) {
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+function changeNickname() {
+    let nickname = prompt('Choose your nickname');
+    if (nickname) {
+        socket.emit('set_nickname', nickname)
+    }
+}
+
+changeNickname();
